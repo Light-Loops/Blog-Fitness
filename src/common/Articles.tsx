3d32332d50681d555efc5a6a4e8260c1dd1b217e
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-09-14 23:49:32 -0500

    In this commit, I updated the navigation buttons in the navbar and made the decision to enhance the code by implementing category-based filtering instead of date-based filtering. The next steps include improving the color scheme and adding icons for the blog.

--- src/common/Articles.tsx
+++ src/common/Articles.tsx
@@ -1,65 +1,68 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Box, Card, CardContent, CardMedia, Container, Typography, Chip, Grid, ButtonGroup, Button } from '@mui/material';
-import { fetchRecentArticles, Article } from '../Api'
-
+import { fetchArticlesByCategory, Article } from '../Api';
 
 export const ArticleList: React.FC = () => {
   const [articles, setArticles] = useState<Article[]>([]);
-  const [filter, setFilter] = useState<string | null>('2d'); // Inicialmente muestra los últimos 2 días
+  const [categoryFilter, setCategoryFilter] = useState<string | null>(null); 
 
-  const fetchArticles = async (timeFilter: string | null) => {
+  const fetchArticles = async (categoryFilter: string | null) => {
     try {
-      const articlesList = await fetchRecentArticles(timeFilter);
+      const articlesList = await fetchArticlesByCategory(categoryFilter);
       setArticles(articlesList);
     } catch (error) {
       console.error(error);
     }
   };
-
+  
   useEffect(() => {
-    fetchArticles(filter);
-  }, [filter]);
+    fetchArticles(categoryFilter);
+  }, [categoryFilter]);
+  
 
   return (
-    <Box maxWidth="lg">
+    <Container maxWidth="lg">
       <Box py={4}>
         <Typography variant="h4" gutterBottom sx={{ color: '#000000' }}>
           Artículos Recientes
         </Typography>
-        <ButtonGroup variant="outlined" sx={{ marginBottom: 2 }}>
-          <Button onClick={() => setFilter('2d')} variant={filter === '2d' ? 'contained' : 'outlined'}>
-            2 días
+        <ButtonGroup  sx={{ marginBottom: 2 }}>
+          <Button onClick={() => setCategoryFilter(null)} variant={categoryFilter === null ? 'contained' : 'outlined'}>
+            Todos
           </Button>
-          <Button onClick={() => setFilter('1w')} variant={filter === '1w' ? 'contained' : 'outlined'}>
-            Semana
+          <Button onClick={() => setCategoryFilter('Nutrición')} variant={categoryFilter === 'Nutrición' ? 'contained' : 'outlined'}>
+            Nutrición
           </Button>
-          <Button onClick={() => setFilter('1m')} variant={filter === '1m' ? 'contained' : 'outlined'}>
-            Mes
+          <Button onClick={() => setCategoryFilter('Entrenamiento')} variant={categoryFilter === 'Entrenamiento' ? 'contained' : 'outlined'}>
+            Entrenamiento
+          </Button>
+          <Button onClick={() => setCategoryFilter('Estilo de Vida')} variant={categoryFilter === 'Estilo de Vida' ? 'contained' : 'outlined'}>
+            Estilo de Vida
           </Button>
         </ButtonGroup>
         <Grid container spacing={3}>
           {articles.map((article) => (
             <Grid key={article.id} item xs={12} sm={6} md={4}>
               <Link to={`/article/${article.id}`} style={{ textDecoration: 'none' }}>
-              <Card sx={{ height: '100%' }}>
-                <CardMedia component="img" height="200" image={article.imageUrl} alt={article.title} />
-                <CardContent>
-                  <Typography variant="h6" gutterBottom>
-                    {article.title}
-                  </Typography>
-                  <Box mt={1}>
-                    {article.tags.map((tag) => (
-                      <Chip key={tag} label={tag} variant="outlined" size="small"  sx={{ marginRight: 1}} />
-                    ))}
-                  </Box>
-                </CardContent>
-              </Card>
+                <Card sx={{ height: '100%' }}>
+                  <CardMedia component="img" height="200" image={article.imageUrl} alt={article.title} />
+                  <CardContent>
+                    <Typography variant="h6" gutterBottom>
+                      {article.title}
+                    </Typography>
+                    <Box mt={1}>
+                      {article.tags.map((tag) => (
+                        <Chip key={tag} label={tag} variant="outlined" size="small" sx={{ marginRight: 1 }} />
+                      ))}
+                    </Box>
+                  </CardContent>
+                </Card>
               </Link>
             </Grid>
           ))}
         </Grid>
       </Box>
-    </Box>
+    </Container>
   );
 };
--- src/common/Navbar.tsx
+++ src/common/Navbar.tsx
@@ -6,31 +6,6 @@ import { Link, useNavigate } from 'react-router-dom';
 
 export const Navbar: React.FC<{}> = () => {
   const navigate = useNavigate();
-  const [mobileMenuAnchor, setMobileMenuAnchor] = useState<null | HTMLElement>(null);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // Agrega un estado para controlar la apertura del menú
-
-  const handleMobileMenuToggle = () => {
-    if (isMobileMenuOpen) {
-      setMobileMenuAnchor(null);
-    } else {
-      setMobileMenuAnchor(document.body);
-    }
-    setIsMobileMenuOpen(!isMobileMenuOpen); // Cambia el estado del menú desplegable
-  };
-
-  const handleNavigation = (path: string) => {
-    navigate(path);
-    handleMobileMenuToggle(); // Cierra el menú al hacer clic en un enlace
-  };
-
-  const navbarLinks = [
-    { label: 'Inicio', path: '/' },
-    { label: 'Nutricion', path: '/nutricion' },
-    { label: 'Entrenamiento', path: '/entrenamiento' },
-    { label: 'Estilo de Vida', path: '/estilo-de-vida' },
-    { label: 'Contacto', path: '/contacto' },
-  ];
-
   return (
     <Box sx={{ flexGrow: 1 }} >
       <AppBar position="sticky" sx={{color: "white", bgcolor: "#272727"}}>
@@ -46,40 +21,6 @@ export const Navbar: React.FC<{}> = () => {
                 <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
                   Fit Way
                 </Typography>
-              </Grid>
-              <Grid item sx={{ display: { xs: 'block', sm: 'none' } }}>
-                <IconButton
-                  size="large"
-                  edge="end"
-                  color="inherit"
-                  aria-label="menu"
-                  onClick={handleMobileMenuToggle}
-                >
-                  {isMobileMenuOpen ? <CloseIcon /> : <MenuIcon />}
-                </IconButton>
-                <Menu
-                  id="mobile-menu"
-                  anchorEl={mobileMenuAnchor}
-                  keepMounted
-                  open={isMobileMenuOpen}
-                  onClose={handleMobileMenuToggle}
-                  anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-                >
-                  {navbarLinks.map((link) => (
-                    <MenuItem key={link.path} onClick={() => handleNavigation(link.path)}>
-                      {link.label}
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </Grid>
-              <Grid item sx={{ display: { xs: 'none', sm: 'block' } }}>
-                {navbarLinks.map((link) => (
-                  <Link key={link.path} to={link.path} style={{ textDecoration: 'none' }}>
-                    <Typography  variant="button" sx={{color:'#e9fff9', mr:'30px'}}>
-                      {link.label}
-                    </Typography>
-                  </Link>
-                ))}
               </Grid>
               <Button
                 variant="contained"

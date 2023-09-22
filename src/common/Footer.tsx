38c4c883152b609c043e0454ef9f454165659092
Abraham Huacchillo <huacchillocastillo@gmail.com>
2023-09-22 16:36:58 -0500

    style:Improved the styles for the footer

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -1,28 +1,30 @@
-import { AppBar, Grid, Toolbar, Typography } from "@mui/material";
-import { Suscribe } from "../components";
-import { Contact } from "../components/Contact";
+import React from 'react';
+import { AppBar, Grid, Toolbar, Typography, Box, Container } from '@mui/material';
+import { Suscribe } from '../components/Suscribe';
+import { Contact } from '../components/Contact';
 
 export const Footer = () => {
   return (
-    <AppBar position="static" component={"footer"} id="footer">
+    <AppBar position="static" component="footer" id="footer">
       <Toolbar>
-        <Grid container>
-          <Grid item xs={12} sm={6}>
-            <Suscribe />
+        <Container maxWidth="xl">
+          <Grid container spacing={2} alignItems="center">
+            <Grid item xs={12} sm={6}>
+              <Suscribe />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Contact />
+              <Box mt={1} textAlign="end">
+                <Typography variant="body1" color="inherit">
+                  &copy; 2023 Fit Way
+                </Typography>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Contact />
-            <Typography
-              variant="body1"
-              color="inherit"
-              padding={1}
-              textAlign={"end"}
-            >
-              &copy; 2023 Fit Way
-            </Typography>
-          </Grid>
-        </Grid>
+        </Container>
       </Toolbar>
     </AppBar>
   );
 };
+
+
